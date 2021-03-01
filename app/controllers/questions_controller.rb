6390d16c93905d0e90c45cedbefe047ee5a849e0
agustina ohan <agustina.oh@gmail.com>
2021-03-01 15:01:03 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,18 @@
 class QuestionsController < ApplicationController
-
   def home; end
-  
-  def ask;end
+
+  def ask; end
+
+  def answer
+    # @answers = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+    @question = params[:question].downcase
+
+    @answer = if @question == 'i am going to work' || @question == "i'm going to work"
+                'Great'
+              elsif params[:question].include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   root to: 'questions#home'
   get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
